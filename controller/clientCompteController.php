de7a559c396384906d00bf9831e0806d5ed5dbcf
MoussaAKHYAR <amrane93.mb@gmail.com>
2020-07-28 10:40:21 +0000

    insertion client et compte

--- controller/clientCompteController.php
+++ controller/clientCompteController.php
@@ -12,8 +12,8 @@ extract($_POST);
 if ($choix_client == 'nouveau') {
   if ($choix_type_client == 'physique') {
     if ($choix_compte == 'simple') {
-      $client = new Personne();//ici on cree un objet 
-      $clientdb = new PersonneModel();// l'objet $clientdb c pour acceder au methode qui se trouve class 
+      $client = new Personne();//ici on cree un objet
+      $clientdb = new PersonneModel();// l'objet $clientdb c pour acceder au methode qui se trouve class
 
       $client->setMatricule(codeAleatoire(8));
       $client->setCin($cni);
@@ -25,42 +25,24 @@ if ($choix_client == 'nouveau') {
       $client->setAdrPersonne($adrPersonne);
       $client->setEmail($email);
 
-     // $ok = $clientdb->addClientPhysique($client);
-     $ok = 1;
-      if ($ok == 1) {
+      $ok = $clientdb->addClientPhysique($client);
+    // $ok = 1;
+      if ($ok) {
         $compte = new Compte();
         $comptedb = new CompteModel();
-
         $compte->setNumero(codeAleatoire(8));
         $compte->setMat($client->getMatricule());
-        $compte->setId(0);
         $compte->setRib(2);
-        $compte->setSolde(0);
-        $dateOuverture = date("Y-m-d");
-        $compte->setDateOuverture($dateOuverture);
-        $compte->setRaisonSocial(null);
-        $compte->setSalaire(0);
-        $compte->setNomEmpl(null);
-        $compte->setTelEmpl(null);
-        $compte->setNumeroIdentification(null);
-        $compte->setAgios(0);
+        $compte->setDateOuverture(date("Y-m-d"));
         $compte->setFraisOuverture(2500);
         $compte->setRemuneration(1000);
-        $compte->setDateDebut(null);
-        $compte->setDateFin(null);
         $compte->setTypeCompte(2);
+        $ok2 = $comptedb->addCompteSimple($compte);
 
-        $ok2 = $comptedb->addCompte($compte);
-        echo $compte->getNomEmpl();
-
-         var_dump($ok2);
-
+        if ($ok2) {
+          header("location:responsable?ok=$ok2");
+        }
       }
-   
-     // var_dump($ok);
-
-      //$adrPersonne = 1;
-      //header("location:responsable?ok=$ok2");
 
     }elseif($choix_compte == 'courant'){
       //$adrPersonne = 1;
--- model/CompteModel.php
+++ model/CompteModel.php
@@ -10,7 +10,7 @@ class CompteModel extends Db{
 
   function addCompte(Compte $compte){
 
-    $sql = "INSERT INTO compte VALUES('{$compte->getNumero()}','{$compte->getMat()}','{$compte->getId()}','{$compte->getRib()}','{$compte->getSolde()}','{$compte->getDateOuverture()}','{$compte->getRaisonSocial()}','{$compte->getSalaire()}','{$compte->getNomEmpl()}','{$compte->getTelEmpl()}','{$compte->getNumeroIdentification()}','{$compte->getAgios()}'{$compte->getFraisOuverture()}','{$compte->getRemuneration()}','{$compte->getDateDebut()}','{$compte->getDateFin()}','{$compte->getTypeCompte()}')";
+    $sql = "INSERT INTO compte   VALUES('{$compte->getNumero()}','{$compte->getMat()}','{$compte->getId()}','{$compte->getRib()}','{$compte->getSolde()}','{$compte->getDateOuverture()}','{$compte->getRaisonSocial()}','{$compte->getSalaire()}','{$compte->getNomEmpl()}','{$compte->getTelEmpl()}','{$compte->getNumeroIdentification()}','{$compte->getAgios()}','{$compte->getFraisOuverture()}','{$compte->getRemuneration()}','{$compte->getDateDebut()}','{$compte->getDateFin()}','{$compte->getTypeCompte()}')";
   
     // si la connexion passe
     try {
@@ -20,6 +20,45 @@ class CompteModel extends Db{
     }
 
   }
+  function addCompteSimple(Compte $compte)
+  {
+    // INSERT INTO `compte`(`numero`, `mat`, `rib`, `dateOuverture`, `fraisOuverture`, `remuneration`,`typeCompte`) VALUES ("147852","14422",12,"2018-02-16",3000,2000,2)
+
+    $sql = "INSERT INTO compte (numero, mat, rib,dateOuverture, fraisOuverture,remuneration,typeCompte) VALUES('{$compte->getNumero()}',
+    '{$compte->getMat()}',
+    {$compte->getRib()},
+    '{$compte->getDateOuverture()}',
+    '{$compte->getFraisOuverture()}',
+    '{$compte->getRemuneration()}',
+    {$compte->getTypeCompte()})";
+
+    // si la connexion passe
+    // try {
+      return $this->db->exec($sql);//on execute la requete
+    // }catch (PDOException $e) {
+    //   $e->getMessage();
+    // }
+
+  }
+
+  function ajouterCompte()
+  {
+    $sql = $this->db->prepare("INSERT INTO compte (numero, mat, rib, dateOuverture, fraisOuverture,remuneration,typeCompte) VALUES(:numero,:mat,:rib,:dateOuverture,:fraisOuverture,:remuneration,:typeCompte)");
+
+    $sql->bindParam(':numero',$numero);
+    $sql->bindParam(':tel',$tel);
+    $sql->bindParam(':email',$email);
+    $sql->bindParam(':login',$login);
+    $sql->bindParam(':password',$password);
+    $sql->bindParam(':adrEntreprise',$adrEntreprise);
+
+    try {
+      return $this->db->exec($sql);//on execute la requete
+    }catch (PDOException $e) {
+      echo $e->getMessage();
+    }
+  }
+  
 
 }
 ?> 
